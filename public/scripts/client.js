a87485ddeaa78281cf725abec8330a2e0729edb4
dburnham1212 <dburnham1212@gmail.com>
2023-06-21 16:56:42 -0400

    Adding ability to see loaded tweets from database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,40 +3,13 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
   for(let tweet of tweets) {
     const $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet); 
+    $('#tweets-container').prepend($tweet); 
   }
 }
 
@@ -54,7 +27,7 @@ const createTweetElement = function(tweet) {
             <p><b>${tweet.content.text}</b></p>
         </div>
         <footer class = "tweet-foot-display">
-          <p>${tweet.created_at}</p>
+          <p>${timeago.format(tweet.created_at)}</p>
           <div>
             <a href="#"><i class="fa-solid fa-flag tweet-icon"></i></a>
             <a href="#"><i class="fa-solid fa-retweet tweet-icon"></i></a>
@@ -76,13 +49,26 @@ const sendPostToBackend = (text) => {
   });
 }
 
+const loadTweets = () => {
+  $.ajax({
+    method: "GET",
+    url: "/tweets/",
+  }).then(res => {
+    renderTweets(res);
+  })
+}
 
 $(document).ready(function() {
   $("#tweet-submit-form").on("submit", event => {
     event.preventDefault();  
-    const result = $(event.target).find("#tweet-text").val();
-    sendPostToBackend(result);
+    const result = $(event.target).find("#tweet-text").val();    
+    if(result.length > 140) {
+      alert("Character limit exceeded");
+    } else {
+      sendPostToBackend(result);
+    }
   });
 
-  renderTweets(data);// to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  loadTweets();
+  //renderTweets(data);// to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
