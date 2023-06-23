af2001e933488fb7c1eecebb2250a3b2d4687190
dburnham1212 <dburnham1212@gmail.com>
2023-06-22 20:09:41 -0400

    Code comments and updating screenshots

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,12 +10,12 @@ const renderTweets = function(tweets) {
   $('#tweets-container').empty(); // Empty the tweet container before display
   for (let tweet of tweets) {
     const $tweet = createTweetElement(tweet);
-    $('#tweets-container').prepend($tweet); 
+    $('#tweets-container').prepend($tweet);
   }
-}
+};
 
 // Function used to validate strings by converting input into a string
-const escapeStr = function (str) {
+const escapeStr = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
@@ -46,7 +46,7 @@ const createTweetElement = function(tweet) {
       </article>
       `);
   return $tweet;
-}
+};
 
 // Post a tweet to the server
 const sendPostToBackend = (text) => {
@@ -57,7 +57,7 @@ const sendPostToBackend = (text) => {
   }).then(() => {//If successful reload the tweets with the new added tweet
     loadTweets();
   });
-}
+};
 
 // Load all of the tweets from the server
 const loadTweets = () => {
@@ -66,8 +66,8 @@ const loadTweets = () => {
     url: "/tweets/",
   }).then((res) => { // If successful render all of the tweets
     renderTweets(res);
-  })
-}
+  });
+};
 
 
 
@@ -84,18 +84,18 @@ $(document).ready(function() {
   backToTop.hide();
 
   /* Add an on click handler to dispay the tweet field if clicked */
-  newTweetButton.on("click", event => {
-    if(tweetArea.is(":visible")) {// If the tweet field is visible hide the field
-      tweetArea.slideUp("slow"); 
+  newTweetButton.on("click", () => {
+    if (tweetArea.is(":visible")) {// If the tweet field is visible hide the field
+      tweetArea.slideUp("slow");
     } else { // Otherwise show the field
-      tweetArea.slideDown("slow"); 
+      tweetArea.slideDown("slow");
     }
   });
 
   /* Check if a user has scrolled and display a button to scroll back to top*/
-  $(document).on("scroll", event => {
-    console.log($(window).scrollTop())
-    if($(window).scrollTop() < 25){// if we have scrolled past a certain threshold show the back to top button an hide the new tweet button
+  $(document).on("scroll", () => {
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() < 25) {// if we have scrolled past a certain threshold show the back to top button an hide the new tweet button
       newTweetButton.show();
       backToTop.hide();
     } else { //Otherwise show the new tweet button and hide the back to top button
@@ -105,20 +105,20 @@ $(document).ready(function() {
   });
 
   /* Check if scroll to top button has been clicked, if so go back to top*/
-  backToTop.on("click", event => {
+  backToTop.on("click", () => {
     tweetArea.slideDown("slow");
     $(window).scrollTop(0);
   });
 
   /* Submit the form data for a new tweet and do error handling */
   $("#tweet-submit-form").on("submit", event => {
-    event.preventDefault();  
+    event.preventDefault();
     const result = $(event.target).find("#tweet-text").val();
-    tweetErrorContainer.slideUp("slow", () => {    
-      if(result.length > characterLimit) { // If the tweet has too many characters
+    tweetErrorContainer.slideUp("slow", () => {
+      if (result.length > characterLimit) { // If the tweet has too many characters
         $("#tweet-error-text").text('TOO LONG, PLEASE RESPECT OUR ARBITRARY POST LENGTH OF 140 CHARACTERS');
         tweetErrorContainer.slideDown("slow");
-      } else if (result.length === 0){ // If there is no body for the tweet
+      } else if (result.length === 0) { // If there is no body for the tweet
         $("#tweet-error-text").text('NO TWEET BODY');
         tweetErrorContainer.slideDown("slow");
       }  else {
